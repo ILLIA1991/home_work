0db6ed72018df2648b9b42f97c89539b6e9e97ee
Pavel Radkevich <pradkevich@eisgroup.com>
2023-04-02 15:43:41 +0300

    ДЗ 19, проверка 1

--- src/main/java/org/example_1/hw_19/Task_1/FootballClub.java
+++ src/main/java/org/example_1/hw_19/Task_1/FootballClub.java
@@ -56,7 +56,7 @@ public class FootballClub {
 
     @Override
     public String toString() {
-        return "FootballClubs{" +
+        return "FootballClub{" +
                 "club='" + club + '\'' +
                 ", country='" + country + '\'' +
                 ", city='" + city + '\'' +
--- src/main/java/org/example_1/hw_19/Task_1/ListClubsExample.java
+++ src/main/java/org/example_1/hw_19/Task_1/ListClubsExample.java
@@ -16,8 +16,12 @@ public class ListClubsExample {
         FootballClub footballClub2 = new FootballClub("Chelsea", "England", "London");
         FootballClub footballClub3 = new FootballClub("Dynamo", "Belarus", "Minsk");
         List<FootballClub> clubArrayList = new ArrayList<>(List.of(footballClub, footballClub1, footballClub2, footballClub3));
+        // Так как нам нужны только страны, использовать Map здесь избыточно и было бы достаточно использовать Set
+        // это также упростило бы код
+        // попробуй переписать на Set
         Map<String, List<FootballClub>> clubsByCountry = new HashMap<>();
 
+        // здесь переменную нужно именовать в единственном числе, не clubs, а club
         for (FootballClub clubs : clubArrayList ) {
             String country = clubs.getCountry();
             if (clubsByCountry.containsKey(country)) {
